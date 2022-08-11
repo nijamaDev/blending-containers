91837d5f33aede5037d1bb85ae2feb8a9cf9f6e7
nijama <nijama.dev@gmail.com>
2022-08-11 00:47:14 -0500

    ups

--- Main.sh
+++ Main.sh
@@ -3,7 +3,7 @@
 ( \
   sleep 1 && Scripts/02_set-guest-kernel.sh && \
   sleep 1 && Scripts/03_set-guest-rootfs.sh && \
-  #sleep 1 && Scripts/04_set-guest-eth0.sh  && \
+  sleep 1 && Scripts/04_set-guest-eth0.sh  && \
   sleep 1 && Scripts/05_start-guest-machine.sh \
 ) & \
 Scripts/01_start-socket.sh
