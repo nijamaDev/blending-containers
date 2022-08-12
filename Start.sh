928549240fd4feb2e4e02968b9984ee9f1983f4d
nijama <nijama.dev@gmail.com>
2022-08-12 10:22:45 -0500

    sudo

--- Start.sh
+++ Start.sh
@@ -1,10 +1,10 @@
 #! /usr/bin/bash
 
 ( \
-  sleep 1 && sudo Scripts/02_set-guest-kernel.sh && \
-  sleep 1 && sudo Scripts/03_set-guest-rootfs.sh && \
+  sleep 1 && Scripts/02_set-guest-kernel.sh && \
+  sleep 1 && Scripts/03_set-guest-rootfs.sh && \
   sleep 1 && Scripts/04_set-guest-eth0.sh   && \
   sleep 1 && Scripts/05_set-cpu-n-mem.sh    && \
   sleep 1 && Scripts/06_start-guest-machine.sh \
 ) & \
-Scripts/01_start-socket.sh
+sudo Scripts/01_start-socket.sh
